5491ea7e968091b31ddf89503fe095398ce1db8a
Naeim <naeim.smz@gmail.com>
2025-06-02 20:05:20 +0430

    creating a redux store

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,7 +14,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "acount/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpos: action.payload.purpos,
+        balance: state.balance + action.payload.amount,
+      };
     case "acount/payLoan":
       return {
         ...state,
@@ -24,3 +31,17 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "acount/deposit", payload: 500 });
+console.log(store.getState());
+store.dispatch({ type: "acount/withdraw", payload: 200 });
+console.log(store.getState());
+
+store.dispatch({
+  type: "acount/requestLoan",
+  payload: { amount: 1000, purpos: "Buy a car" },
+});
+
+console.log(store.getState());
